eed7a62c0762d6e56565d6135eafbf5e6adce0f9
ShaneDsouza4 <shane.json96@gmail.com>
2024-06-20 18:33:19 -0400

    Project Setup

---
+++ models/url.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+//Schema
+const urlSchema = new mongoose.Schema({
+    shortId:{
+        type: String,
+        required: true,
+        unique: true
+    },
+    redirectURL:{
+        type:String,
+        required: true
+    },
+    visitHistory:[{
+        timestamp: {type: Number}
+    }]
+}, {timestamps: true});
+
+//Model
+const URL = mongoose.model('url', urlSchema);
+
+//Exports
+module.exports = URL;
