2b62436011ff4a92e8866283bf4ca8f7217a0bb7
ShaneDsouza4 <shane.json96@gmail.com>
2024-06-21 15:04:53 -0400

    Server side rendering added

--- controllers/url.js
+++ controllers/url.js
@@ -4,7 +4,6 @@ const URL = require("../models/url");
 
 async function handleGenerateNewShortURL(req, res){
     const body = req.body;
-    console.log("--->", body)
     if(!body.url){
         return res.status(400).json({error:"URL is required."});
     }
@@ -17,25 +16,26 @@ async function handleGenerateNewShortURL(req, res){
         visitHistory: []
     })
 
-    return res.json({ id: shortID });
+    return res.render("home", {
+        id: shortID
+    })
+    //return res.json({ id: shortID });
 }
 
 
 async function handleGetShortID(req, res){
-    console.log()
+
     const shortID = req.params.shortID;
     const entry = await URL.findOneAndUpdate(
         {shortID: shortID}, //Find
         { $push:{visitHistory: { timestamp: Date.now() }}} //Update
     )
 
-    res.redirect(entry.redirectURL)
-
+    res.redirect(entry?.redirectURL) //Redirect to URL
 }
 
 async function handleGetAnalytics(req, res){
     const shortID = req.params.shortID;
-
     const result = await URL.findOne({shortID});
 
     return res.json({totalClicks: result.visitHistory.length , analytics: result.visitHistory});
--- index.js
+++ index.js
@@ -1,16 +1,28 @@
 const express = require("express");
 const urlRoutes = require("./routes/url");
 const { connectToMongoDB } = require("./connection");
+const PORT = 8000;
+const URL = require("./models/url");
+const path = require("path");
+
+const staticRoute = require("./routes/staticRouter")
+
 
 const app = express();
-app.use(express.json())
-
-const PORT = 8000;
+app.use(express.json()); //JSON Data
+app.use(express.urlencoded({extended: false})); //Form Data
 
 connectToMongoDB("mongodb://127.0.0.1:27017/urlShortener")
 .then(()=>console.log("MongoDB connected."));
 
-app.use("/", urlRoutes);
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
+
+
+app.use("/", staticRoute);
+
+app.use("/url", urlRoutes);
+
 
 
 app.listen(PORT, ()=> console.log(`Server started at Port:${PORT}`));
